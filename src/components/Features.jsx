b890bdcdded35cd8723fb6efb1e1aa315399d67d
dclassicgenius <patamaechi1@gmail.com>
2024-07-05 09:40:39 +0300

    apple site

---
+++ src/components/Features.jsx
@@ -0,0 +1,13 @@
+const Features = () => {
+  return (
+    <section>
+      <div className="screen-max-width">
+        <div className="mb-12 w-full">
+          <h1>Explore the full story.</h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
